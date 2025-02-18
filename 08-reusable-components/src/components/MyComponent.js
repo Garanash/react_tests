6004e190a92899ceebfc726865316f6833aafd60
Andrew Michaelson <90028419+Garanash@users.noreply.github.com>
2025-02-18 16:35:37 +0300

    reusable added

---
+++ 08-reusable-components/src/components/MyComponent.js
@@ -0,0 +1,10 @@
+function MyComponent() {
+    return (
+        <div>
+      <h1>Hello React reusable</h1>
+      <button>like!</button>
+      </div>
+    );
+  }
+  
+  export default MyComponent;
